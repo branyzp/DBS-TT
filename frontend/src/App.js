c5bcc4195f118e49de840fa70599ea979f49aba3
Brandon <branyzp@gmail.com>
2023-02-25 10:42:30 +0800

    add login form and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route />
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
+// import { useAuth } from '../contexts/AuthContext';
 
 function Login() {
-	return <div>Login</div>;
+	const [email, setemail] = useState({ email: '', dirty: false });
+	const [password, setpassword] = useState({ password: '', dirty: false });
+
+	// const auth = useAuth();
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// 	auth.Login(email, password).then(
+		// 		() => {
+		// 			navigate('/');
+		// 		},
+		// 		(err) => {
+		// 			console.log(err);
+		// 		}
+		// 	);
+	};
+
+	return (
+		<div className="formcard">
+			<h1>Login Form</h1>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						onChange={(e) => {
+							setemail({ email: e.target.value, dirty: true });
+							console.log(email);
+						}}
+						type="email"
+						placeholder="Enter email"
+					/>
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						onChange={(e) => {
+							setpassword({ password: e.target.value, dirty: true });
+							console.log(password);
+						}}
+						type="password"
+						placeholder="Password"
+					/>
+				</Form.Group>
+
+				<Button variant="primary" type="submit">
+					Login
+				</Button>
+			</Form>
+		</div>
+	);
 }
 
 export default Login;
