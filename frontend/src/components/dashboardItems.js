8e8022797a83ac9ba3bd7aed90d49a1d8c478c14
Darren Loh <vanhfdarren@gmail.com>
2023-02-25 10:33:32 +0800

    initial dashboard changes

---
+++ frontend/src/components/dashboardItems.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function dashboardItems() {
+  return (
+    <div>
+      <div className='dashboardItemsBody'>
+        
+      </div>
+      <div className='dashboardItemsButtons'></div>
+    </div>
+  )
+}
+
+export default dashboardItems
---
+++ frontend/src/components/dashboardPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function dashboardPage() {
+  return (
+    <div className='mainContainer'>
+
+        <div className='dashboardMainHeader'></div>
+        <div className='dashboardMainBody'></div>
+      
+    </div>
+  )
+}
+
+export default dashboardPage
